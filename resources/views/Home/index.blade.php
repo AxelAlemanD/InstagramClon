de1b0282d11b1c996f89b401c70ba9c84e5defaa
JuanNava897 <1930536@upv.edu.mx>
2022-04-05 23:06:56 -0500

    home view navbar

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -1,17 +1,66 @@
+<style>
+    :root{
+        --white: #ffffff;
+        --grey: #fafafa;
+    }
+    .bg-grey {
+        background-color: var(--grey);
+    }
+    .bg-light {
+        background-color: var(--light)!important;
+    }
+    .navbar{
+        min-height: 5rem;
+    }
+    i.lead {
+        font-size: 1.8rem;
+    }
+    .form-control::focus{
+        box-shadow: none;
+    }
+</style>
+
+
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/99852e9fe2.js" crossorigin="anonymous"></script>
 
-    <title>Instagram Clone</title>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
+        <title>Instagram Clone</title>
+    </head>
+    <body class="bg-grey">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+    <div class="container d-flex justify-content-between">
+        <div>
+            <a class="navbar-brand border-right pr-4" href="#"><i class="fa-brands fa-instagram lead"></i></a>
+            <img src="images/insta.png" class="img-fluid w-50">
+        </div>
+        <form class="d-flex position-relative">
+            <input class="form-control bg-grey border text-center mr-2" type="Search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-link text-muted position-absolute" type="submit"><i class="fas fa-search"></i></button>
+        </form>
+
+        <div class="collapse navbar-collapse w-auto" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link"  href="#"><i class="far fa-compass lead text-dark mr-3"></i></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link"  href="#"><i class="far fa-heart lead text-dark mr-3"></i></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link"  href="#"><i class="far fa-user lead text-dark"></i></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
